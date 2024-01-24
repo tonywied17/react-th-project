3e7600702e3363d730d7b7f7466d0b9c0bd42f90
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2024-01-24 13:51:06 -0500

    door hinge border animation, about card, container widths

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Gallery() {
   return (
-    <div className='container mx-auto fade-center'>
+    <div className='container mx-auto fade-center lg:px-7'>
       <h2 class="md:text-4xl text-2xl font-extrabold text-[#313233] my-5">Instagram Gallery</h2>
       <p class="mt-4 mb-8 md:text-lg text-base text-[#47494a]">Explore our curated collection of inspiring moments on Instagram. Follow us for the latest updates!</p>
     </div>
--- src/components/Manufacturers.js
+++ src/components/Manufacturers.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Manufacturers() {
   return (
-    <div className='container mx-auto fade-center'>
+    <div className='container mx-auto fade-center lg:px-7'>
       <h2 class="md:text-4xl text-2xl font-extrabold text-[#313233] my-5">Our Manufacturers</h2>
       <p class="mt-4 mb-8 md:text-lg text-base text-[#47494a]">Discover our trusted partners and manufacturers who contribute to the quality and excellence of our projects.</p>
     </div>
